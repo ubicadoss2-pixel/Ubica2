ff70ba5aac5c5447b7e46475795be0a8a594bd7f
Laura <lauracatanopalacio8@gmail.com>
2026-07-01 16:15:59 -0500

    fix: corrige error 400 en creacion de lugares, unifica image uploader y mejora UI de horarios

--- src/app.ts
+++ src/app.ts
@@ -26,8 +26,12 @@ import { errorHandler, notFound } from "./shared/middlewares/error.middleware";
 
 const app = express();
 
-app.use(cors());
-app.use(helmet());
+app.use(cors({ origin: "*" }));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
